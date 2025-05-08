65b4a8b85321dd1dc7f498f445e44ed43004b673
ywiis <solejone43@gmail.com>
2025-05-08 14:50:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 cors_proxy.createServer({
-  originWhitelist: [], // السماح لكل النطاقات
-  requireHeader: [],
-  removeHeaders: ['cookie', 'cookie2'],
-  // إضافة رؤوس CORS صريحة لأي طلب
-  setHeaders: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE',
-    'Access-Control-Allow-Headers': '*',
-  }
+    originWhitelist: [], // السماح لجميع النطاقات
+    requireHeader: [],
+    removeHeaders: ['cookie', 'cookie2'],
+    setHeaders: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE',
+        'Access-Control-Allow-Headers': '*',
+    }
 }).listen(port, host, () => {
-  console.log(`CORS Proxy running on http://${host}:${port}`);
+    console.log(`CORS Proxy running on http://${host}:${port}`);
 });
